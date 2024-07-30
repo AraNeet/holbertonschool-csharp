427bf37aa0309fe0c457e1e77aabccb38fb4bbc3
AramisAra <martinezcruzaj@gmail.com>
2024-07-30 11:02:52 -0400

    Adding name of file because they had .csproj

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -d "1-new_project" ]; then
-    dotnet new console -n 1-new_project.csproj
+    dotnet new console -n 1-new_project
 fi
 cd 1-new_project
-dotnet build 1-new_project.csproj
+dotnet build 1-new_project
--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [ ! -d "2-new_project" ]; then
-    dotnet new console -n 2-new_project.csproj
+    dotnet new console -n 2-new_project
 fi
 cd 2-new_project
-dotnet build 2-new_project.csproj
-dotnet run 2-new_project.csproj
+dotnet build 2-new_project
+dotnet run 2-new_project
