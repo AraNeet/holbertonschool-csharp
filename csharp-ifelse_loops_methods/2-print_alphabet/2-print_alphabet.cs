2cbd73b55b183a116276c5cf7fbed1ac61337a82
AramisAra <martinezcruzaj@gmail.com>
2024-07-31 10:16:34 -0400

    Completing task 2

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        char abc;
+        for (abc = 'a'; abc <= 'z'; abc++)
+        {
+            Console.Write("{0}", abc);
+        }
+    }
+}
