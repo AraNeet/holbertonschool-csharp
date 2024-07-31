d270f63b414bb18e19c6aef9c7d0be57e99623ad
AramisAra <martinezcruzaj@gmail.com>
2024-07-31 10:44:55 -0400

    Completing task 4

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int num;
+        for (num = 0; num <= 98; num++)
+        {
+            Console.Write("{0} = 0x{1}\n", num, num.ToString("x"));
+        }
+    }
+}
