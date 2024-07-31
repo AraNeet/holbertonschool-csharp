ecb43045103870383846e2a8975ceccc511ecdbe
AramisAra <martinezcruzaj@gmail.com>
2024-07-31 11:34:13 -0400

    Completing task 5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int num;
+        for (num = 0; num <= 99; num++)
+        {
+            if (num <= 9)
+            {
+                Console.Write("0{0}, ", num);
+            }
+            else
+            {
+                if (num == 99)
+                {
+                    Console.Write("{0}\n", num);
+                }
+                else
+                {
+                    Console.Write("{0}, ", num);
+                }
+            }
+        }
+    }
+}
