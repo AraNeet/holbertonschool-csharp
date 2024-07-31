cfae5cc167d94e72408720aa79b0bbfc3bdb6956
AramisAra <martinezcruzaj@gmail.com>
2024-07-31 10:29:28 -0400

    Completing the 3 task

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        char abc;
+        for (abc = 'a'; abc <= 'z'; abc++)
+        {
+            if (abc != 'q' && abc != 'e')
+            {
+                Console.Write("{0}", abc);
+            }
+        }
+    }
+}
