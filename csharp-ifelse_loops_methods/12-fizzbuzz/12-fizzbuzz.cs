51021cbf848c5c92f87dc416695318fec1a07cdf
AramisAra <martinezcruzaj@gmail.com>
2024-08-01 15:34:40 -0400

    Adding a main and the fizz buzz task

---
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int num;
+        for (num = 0; num <= 100; num++)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                Console.Write("FizzBuzz ");
+            }
+            else if (num % 3 == 0)
+            {
+                Console.Write("Fizz ");
+            }
+            else if (num % 5 == 0)
+            {
+                Console.Write("Buzz ");
+            }
+            else
+            {
+                Console.Write("{0} ", num);
+            }
+        }
+        Console.WriteLine("");
+    }
+}
