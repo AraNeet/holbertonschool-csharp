91efb64faaadff01ecb729afff4c96d283e51dba
AramisAra <martinezcruzaj@gmail.com>
2024-08-01 15:00:44 -0400

    Adding the 10 task

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        int x;
+        if (length <= 0)
+        {
+            Console.Write("\n");
+        }
+        else
+        {
+            for (x = 0; x <= length; x++)
+            {
+                if (x == length)
+                {
+                    Console.Write("\n");
+                }
+                else if (x < length)
+                {
+                    Console.Write("{0}", x.ToString("_"));
+                }
+            }
+        }
+    }
+}
