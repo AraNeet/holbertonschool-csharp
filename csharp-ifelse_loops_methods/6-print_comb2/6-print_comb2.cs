3cbffb452b6bdebd27b206feb882923edd8044a3
AramisAra <martinezcruzaj@gmail.com>
2024-08-01 11:40:17 -0400

    Completing task 6

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int numa; 
+        int numb = 0;
+        for (numa = 0; numa <= 8; numa++)
+        {
+            for (numb = numa + 1; numb <= 9; numb++)
+            {
+                if (numa == 8 && numb == 9)
+                {
+                    Console.Write("{0}{1}\n", numa, numb);
+                }
+                else 
+                {
+                    Console.Write("{0}{1}, ", numa, numb);
+                }
+            }
+        }
+    }
+}
