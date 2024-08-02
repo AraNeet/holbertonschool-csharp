251fb8658b728fe4a67c9088666791e963bb2093
AramisAra <martinezcruzaj@gmail.com>
2024-08-02 09:54:14 -0400

    Adding the 0 task

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size) {
+        if (size < 0) {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        } else if (size == 0) {
+            Console.WriteLine();
+            return new int[0];
+        } else {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = i;
+            }
+            for (int a = 0; a < array.Length; a++) {
+                Console.Write(a + " ");
+            }
+            Console.WriteLine();
+            return array;
+        }
+    }
+}
