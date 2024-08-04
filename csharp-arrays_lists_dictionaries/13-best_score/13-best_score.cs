de70ba629d6419260a7eb54165dfacdabb799aaf
AramisAra <martinezcruzaj@gmail.com>
2024-08-04 01:22:14 -0400

    Adding task 1-13

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myDict)
+    {
+        if (myDict.Count == 0)
+        {
+            return "None";
+        }
+        string player = "";
+        int BestScore = 0;
+        foreach (KeyValuePair<string, int> score in myDict)
+        {
+            if (BestScore < score.Value)
+            {
+                player = score.Key;
+                BestScore = score.Value;
+            }
+        }
+        return player;
+    }
+}
---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> list = new List<int>();
+        for (int i = 0; i < size; i++)
+        {
+            list.Add(i);
+            if (i == size - 1)
+            {
+                Console.Write("{0}", i);
+            }
+            else
+            {
+                Console.Write("{0} ", i);
+            }
+        }
+        Console.WriteLine();
+        return list;
+    }
+}
