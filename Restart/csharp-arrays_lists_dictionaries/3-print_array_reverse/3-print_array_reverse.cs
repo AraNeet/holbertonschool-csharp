97ea3f1923a5d678777a2be52d25801bd5e1c5ce
Aramis Martinez <135170950+AramisAra@users.noreply.github.com>
2024-11-19 22:26:03 -0400

    Adding the new version of the project

---
+++ Restart/csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null)
+        {
+            Console.WriteLine();
+            return;
+        }
+        for (int index = array.Length - 1; index >= 0; index--)
+        {
+            Console.Write(array[index] + " ");
+        }
+        Console.WriteLine();
+    }
+}
