6094524729f1b677c627fe775fff5ff87a8ae0fe
Aramis Martinez <135170950+AramisAra@users.noreply.github.com>
2024-11-19 22:04:23 -0500

    Adding the 5 question. Need to fix it not print the full number.

---
+++ Restart/csharp-arrays_lists_dictionaries/5-max_integer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> MyList)
+    {
+        if (MyList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+        int greatest = MyList[0];
+        for (int i = 0; i < MyList.Count; i++)
+        {
+            if (MyList[i] > greatest)
+            {
+                greatest = i;
+            }
+        }
+
+        return greatest;
+    }
+}
