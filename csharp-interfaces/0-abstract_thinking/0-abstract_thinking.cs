bfe107162328d085dbeb155f1e3e238743eb4afc
Aramis Martinez <135170950+AramisAra@users.noreply.github.com>
2024-12-04 10:50:45 -0400

    adding all the missing C# project.

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Base class
+/// </summary>
+abstract class Base {
+
+    public string name { get; set; }
+
+    /// <summary>
+    ///  ToString method
+    /// </summary>
+    /// <returns>String</returns>
+    public override string ToString() {
+        return $"{name} is a {this.GetType()}";
+    }
+
+}
---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+///<summary>
+/// Base class
+/// </summary>
+abstract class Base
+{
+  public string name = "";
+
+	public override string ToString()
+	{
+		return (this.name + " is a " + this.GetType().ToString());
+	}
+}
+
+/// <summary>
+/// Interactive interface
+/// </summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary>
+/// Breakable Interface
+/// </summary>
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+/// <summary>
+/// Collectable Interface
+/// </summary>
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+
+}
+
+/// <summary>
+/// TestObject class
+/// </summary>
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    /// <summary>
+    /// Takes place when the object interacts.
+    /// </summary>
+    public void Interact()
+    {
+        // Method implementation
+    }
+
+    /// <summary>
+    /// Takes place when the object breaks.
+    /// </summary>
+    public void Break()
+    {
+        // Method implementation
+    }
+
+    /// <summary>
+    /// Takes place when the object collects something.
+    /// </summary>
+    public void Collect()
+    {
+        // Method implementation
+    }
+}
