34b3a30c69964809d31ca5f850c44c33d11ba721
Aramis Martinez <135170950+AramisAra@users.noreply.github.com>
2025-03-21 21:38:27 -0300

    Adding task one in text_based_interface

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,9 @@
+﻿using System.DateTimestructure;
+namespace InventoryLibrary;
+
+public class BaseClass
+{
+    public string id;
+    public DateTime date_created;
+    public DateTime date_updated;
+}
